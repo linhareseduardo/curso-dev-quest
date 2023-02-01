a2085701fcf47cc02baf3d39e332146793be2a79
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-01 20:26:01 -0300

    aula 05 operadores logicos

---
+++ modulo-javascript-basico/05-operadores-logico/index.js
@@ -0,0 +1,23 @@
+/* 
+    == -----igual----- a==b ---- verdadeiro se 'a' for igual a 'b'
+    != ----- não é igual (diferente) - verdadeiro se 'a' não for igual a 'b'
+    === ------ identico----- a===b ----- verdadeiro se 'a' for identico a 'b'
+    !== ----- não identico a!==b verdadeiro se 'a' não for identico a 'b'
+    < ----- menor que a < b ---- verdadeiro quando 'a' for menor que 'b'
+    > ------ maior que a > b ----- verdadeiro quando 'a' for maior que 'b'
+    >= maior ou igual que a >= b ---- verdadeiro quando 'a' for maior ou igual a 'b'
+    <= menor ou igual que a <= b ----- verdadeiro quando 'a' for menor ou igual a 'b'
+*/
+
+
+const a = 3
+const b = '3'
+
+console.log(a!==b)
+console.log(a===b)
+console.log(a==b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a>=b)
+console.log(a<b)
+console.log(a<=b)
