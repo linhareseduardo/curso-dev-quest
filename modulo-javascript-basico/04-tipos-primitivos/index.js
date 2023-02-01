c4ba8439380adc12261944b40687da751f674fb3
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-01 15:02:58 -0300

    aula 04 tipos primitivos

---
+++ modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,23 @@
+/*
+    String
+    Number
+    Boolean
+    Null
+    Undefined
+    Simbol
+
+*/
+
+let nome = 'Roberto'
+let numero = 10
+let usuarioVerificado = false
+
+
+let pessoa = null
+
+
+if(usuarioVerificado){
+    console.log("Usuário verificado")
+}else{
+    console.log("Usuário não é verificado")
+}
