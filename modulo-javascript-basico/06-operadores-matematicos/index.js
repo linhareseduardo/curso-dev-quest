e54416cddc798d48f31b76f09cd2a826f2abbc2f
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-02 16:37:41 -0300

    aula 06 operadores matematicos

---
+++ modulo-javascript-basico/06-operadores-matematicos/index.js
@@ -0,0 +1,10 @@
+let primeiroValor = 4
+let segundovalor = 2
+
+console.log(primeiroValor + segundovalor)
+console.log(primeiroValor - segundovalor)
+console.log(primeiroValor / segundovalor)
+console.log(primeiroValor * segundovalor)
+console.log(primeiroValor % segundovalor)
+console.log(primeiroValor ** segundovalor)
+
