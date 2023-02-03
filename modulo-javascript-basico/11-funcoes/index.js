304b6e7f27b5e35d9bbfd89b0884f55fddf5c8a8
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-02 22:05:39 -0300

    aula 11 funções

---
+++ modulo-javascript-basico/11-funcoes/index.js
@@ -0,0 +1,38 @@
+/*
+    function nomeDaFuncao(){
+        <bloco de execução>
+    }
+
+    nomeDaFuncao()
+*/
+
+/*
+    function nomeDaFuncao(parametro1, parametro2){
+        <bloco de execução>
+    }
+
+    nomeDaFuncao(parametro1, parametro2)
+*/
+
+function incentivarQuester(){
+    alert('Você chegou ao JS continue assim')
+}
+
+incentivarQuester()
+
+
+function soma(numero1, numero2){
+    console.log(numero1 + numero2)
+}
+
+soma(10,2)
+
+let resultadoDaSoma = soma(1,3)
+
+console.log(resultadoDaSoma)
+
+function incentivarQuester2(nomeQuester){
+    alert('Você chegou ao JS '+ nomeQuester +' continue assim')
+}
+
+incentivarQuester2('Roberto')
