42290dd1adc603cfd9d6d404b9b1104f5679aaa5
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-02 21:27:49 -0300

    aula 08 laços parte 02 while e do while

--- modulo-javascript-basico/07-lacos/index.js
+++ modulo-javascript-basico/07-lacos/index.js
@@ -5,3 +5,25 @@ for(let i = 0; i < 10; i++){
 for(let i = 0; i < 10; i++){
     console.log('Hello world')
 }
+
+let i = 10
+while(i > 0){
+    console.log(i)
+    i--
+}
+
+let a = 10
+let b = 15
+
+while(a < b){
+    console.log('Incrementando a variável a ' + a)
+    a++
+}
+
+a = 10
+b = 15
+
+do{
+    console.log('Incrementando a variável a seu valor agora é: ' + a)
+    a++
+}while(a < b)
