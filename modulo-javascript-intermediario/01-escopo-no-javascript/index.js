df83637b9194a8b12f81104ca9bb7a3cb7985c29
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-03 16:34:15 -0300

    criação do módulo javascript intermediário e aula 01 escopo no javascript

---
+++ modulo-javascript-intermediario/01-escopo-no-javascript/index.js
@@ -0,0 +1,26 @@
+// if(true){
+//     const mensagem = 'Olá'
+// }
+
+// console.log(mensagem)
+
+// {
+//     var mensagem = 'Olá'
+//     console.log(mensagem)
+// }
+
+// console.log(mensagem)
+
+function falar(){
+    var mensagem = 'Olá mundo'
+    console.log(mensagem)
+
+    function falarOutraCoisa(){
+
+    }
+    falarOutraCoisa()
+    console.log(falarOutraCoisa)
+}
+
+falar()
+console.log(mensagem)
