4abfd98d60d8c0b4551ed52b54f5a57b495fd100
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-02 21:38:51 -0300

    aula 09 condicionais if, else, else if, e operador ternário

---
+++ modulo-javascript-basico/09-condicionais/index.js
@@ -0,0 +1,14 @@
+let nomeFilme = 'Vingadores'
+
+if(nomeFilme === 'Vingadores'){
+    console.log('É o filme dos Vingadores')
+}else if(nomeFilme === 'Batman'){
+    console.log('É o filme do Batman')
+}else{
+    console.log('É outro filme')
+}
+
+//operador ternário
+//condição ? console.log('true') : console.log('false')
+
+nomeFilme === 'Vingadores' ? console.log('É o filme dos vingadores') : console.log('É outro filme')
