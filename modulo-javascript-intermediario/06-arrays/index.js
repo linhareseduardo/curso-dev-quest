1e39cbe6a6aefcdfe383b2bb378343b671417448
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-03 21:45:13 -0300

    aula 06 arrays

---
+++ modulo-javascript-intermediario/06-arrays/index.js
@@ -0,0 +1,12 @@
+let filmes = ['Batman', 'Vingadores', 'Hulk', 'Spirit']
+
+let avaliacoes = [1, 4, 6, 5]
+
+console.log(filmes[0])
+
+let descricaoFilme = ['Vingadores', 4.5, true]
+
+console.log(descricaoFilme)
+
+console.log(filmes[12])
+
