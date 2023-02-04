df4698003f2a735e98168952ddc55b4e90880ffc
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-03 21:18:26 -0300

    aula 04 arrow functions

---
+++ modulo-javascript-intermediario/04-arrow-functions/index.js
@@ -0,0 +1,16 @@
+let resultadoDaSoma = (numero1,numero2) => {
+    return numero1 + numero2
+}
+
+console.log(resultadoDaSoma(12,25))
+
+const incentivarQuester = nomeQuester => {
+    console.log('Parabéns '+ nomeQuester + ', você chegou ao módulo intermediário')
+}
+
+incentivarQuester('Eduardo')
+
+
+let resultadoDaSubtracao = (numero1, numero2) => numero1 - numero2
+
+console.log(resultadoDaSubtracao(10,200))
