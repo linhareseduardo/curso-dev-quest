f22f511c2554b1d9947ca15fc3a325b1e4f2d708
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-07 14:30:52 -0300

    aula 16 selecionando elementos dom

---
+++ modulo-javascript-intermediario/16-selecionando-elementos-dom/index.js
@@ -0,0 +1,17 @@
+
+// let p = document.getElementById('paragrafo1')
+
+// console.log(p)
+
+
+// let inputPorClasse = document.getElementsByClassName('tag-de-input')
+
+// console.log(inputPorClasse[0])
+
+// let inputPorTag = document.getElementsByTagName('input')
+
+// console.log(inputPorTag[0])
+
+let inputPorName = document.querySelectorAll("input[name='email']")
+
+console.log(inputPorName)
