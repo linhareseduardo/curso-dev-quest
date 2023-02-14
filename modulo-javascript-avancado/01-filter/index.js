a98771e26f8b304c1cb6613ef16b00a8f9af92c4
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-14 10:15:11 -0300

    criação do módulo javascript avançado e aula 01 - filter

---
+++ modulo-javascript-avancado/01-filter/index.js
@@ -0,0 +1,22 @@
+let pessoas =[
+    {nome: 'Roberto', idade: 33},
+    {nome: 'Ricardo', idade: 33},
+    {nome: 'Eduardo', idade: 26}
+]
+
+// let pessoasComIdadeDe33Anos = []
+// for(let i = 0; i < pessoas.length; i++){
+//     if(pessoas[i].idade === 33){
+//         pessoasComIdadeDe33Anos.push(pessoas[i])
+//     }
+// }
+
+// console.log(pessoasComIdadeDe33Anos)
+
+// let pessoasComIdadeDe33Anos = pessoas.filter(function(pessoa){return pessoa.idade === 33})
+
+// console.log(pessoasComIdadeDe33Anos)    
+
+let pessoasComIdadeDe33Anos = pessoas.filter((pessoa) => pessoa.idade === 33)
+
+console.log(pessoasComIdadeDe33Anos)
