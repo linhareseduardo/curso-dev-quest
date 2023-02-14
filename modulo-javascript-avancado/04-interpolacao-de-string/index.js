f79b7907b7427f67fef7a147c75a6693f150087c
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-14 11:00:27 -0300

    aula 04 - interpolação de string

---
+++ modulo-javascript-avancado/04-interpolacao-de-string/index.js
@@ -0,0 +1,19 @@
+// const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+//     return 'Primeiro Nome: ' + primeiroNome + ', Último nome: ' + ultimoNome + ', Idade: ' + idade
+// }
+
+// console.log(imprimirInformacoesPessoa('Roberto', 'Dias', 33))
+
+
+const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro nome: ${primeiroNome}, último nome: ${ultimoNome}, idade: ${idade}`
+}
+
+// console.log(imprimirInformacoesPessoa('Roberto', 'Dias', 33))
+
+console.log(`${imprimirInformacoesPessoa('Roberto', 'Dias', 33)}, ele é um DevQuester`)
+
+console.log(`Soma ${10+20}`)
+
+
+
