a3327b6458a9a7fdd8ebe3f12a1d2dcca6e0ea86
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-14 10:43:33 -0300

    aula 03 - reduce

---
+++ modulo-javascript-avancado/03-reduce/index.js
@@ -0,0 +1,24 @@
+let ordens = [
+    {cliente: 'Roberto', tipo: 'compra', quantidade: 56, ativo: 'NFLX34'},
+    {cliente: 'Ricardo', tipo: 'compra', quantidade: 76, ativo: 'AAPL34'},
+    {cliente: 'Ruan', tipo: 'venda', quantidade: 21, ativo: 'GOGL34'}
+]
+
+// let quantidadeDeOrdens = 0
+
+// for (let i = 0; i < ordens.length; i++){
+//     quantidadeDeOrdens += ordens[i].quantidade
+// }
+
+// console.log(quantidadeDeOrdens)
+
+// let quantidadeDeOrdens = ordens.reduce(function(somaOrdens, ordem){
+//     console.log('Ordem', somaOrdens, ordem)
+//     return somaOrdens + ordem.quantidade
+// },0)
+
+// console.log(quantidadeDeOrdens)
+
+let quantidadeDeOrdens = ordens.reduce((somaOrdens, ordem) => somaOrdens + ordem.quantidade, 0)
+
+console.log(quantidadeDeOrdens)
