6c6c6aa89eb33cac154fd899c34f65259fe557c8
linhareseduardo <linhareseduardo7@gmail.com>
2023-02-14 13:36:39 -0300

    aula 05 desestruturação objetos arrays

---
+++ modulo-javascript-avancado/05-desestruturacao-objetos-arrays/index.js
@@ -0,0 +1,24 @@
+let pessoa = {
+    nome: 'Roberto',
+    sobrenome: 'Dias',
+    idade: 33
+}
+
+// let nome = pessoa.nome
+// let idade = pessoa.idade
+
+// console.log(nome)
+// console.log(idade)
+
+// let { nome: nome, idade: idade, sobrenome: sobrenome } = pessoa
+
+// console.log(nome,idade,sobrenome)
+
+
+const numeros = [1, 2, 3]
+
+const [um, dois, tres] = numeros
+
+console.log(um)
+console.log(dois)
+console.log(tres)
